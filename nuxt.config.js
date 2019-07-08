558c1c09e6d4a9b3c6ac32fa122e3fb9b751487c
jay-g <jay-g.lee@nexr.com>
2019-07-08 17:33:39 +0900

    for test

---
+++ nuxt.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    {src: '~/plugins/echarts/echarts', ssr: false},
+    {src: '~/plugins/vue_draggable_resizable/vue-draggable-resizable', ssr: false},
+    // {src: '~/plugins/echarts'},
+    // {src:'~/plugins/vue-echarts', ssr:false}
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+      transpile: ['vue-echarts', 'resize-detector'],
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/echarts/index.vue
@@ -0,0 +1,198 @@
+<template>
+  <div id="app">
+    <!--<div id="charts" style="height:70vh;"></div>-->
+    <!--<vue-draggable-resizable :grid="[20,20]" :x="10" :y="10">-->
+    <!--</vue-draggable-resizable>-->
+    <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
+      <vue-draggable-resizable :w="100" :h="100" @dragging="onDrag" @resizing="onResize" :parent="true">
+        <div id="charts" style="height:70vh;"></div>
+      </vue-draggable-resizable>
+    </div>
+  </div>
+</template>
+<script>
+  import echarts from 'echarts'
+
+  export default {
+    data () {
+      return  {
+        myChart: null,
+        option: {},
+        graph: {
+          nodes: [
+            {
+              name: 'Node1',
+              x: 300,
+              y: 300
+            },
+            {
+              name: 'Node2',
+              x: 800,
+              y: 300
+            },
+            {
+              name: 'Node3',
+              x: 550,
+              y: 100
+            },
+            {
+              name: 'Node4',
+              x: 550,
+              y: 500
+            }
+          ],
+          links: [
+            {
+              source: 0,
+              target: 1,
+              symbolSize: [5, 20],
+              label: {
+                normal: {
+                  show: true
+                }
+              },
+              lineStyle: {
+                normal: {
+                  width: 5,
+                  curveness: 0.2
+                }
+              }
+            },
+            {
+              source: 'Node2',
+              target: 'Node1',
+              label: {
+                normal: {
+                  show: true
+                }
+              },
+              lineStyle: {
+                normal: { curveness: 0.2 }
+              }
+            },
+            {
+              source: 'Node1',
+              target: 'Node3'
+            },
+            {
+              source: 'Node2',
+              target: 'Node3'
+            },
+            {
+              source: 'Node2',
+              target: 'Node4'
+            },
+            {
+              source: 'Node1',
+              target: 'Node4'
+            }
+          ]
+        }
+      }
+    },
+    mounted () {
+      this.initChart();
+      this.renderChart();
+    },
+    methods: {
+      initChart () {
+        this.myChart = echarts.init(document.getElementById('charts'));
+      },
+      renderChart() {
+        this.myChart.showLoading();
+        this.formatOption();
+        this.initDrag();
+      },
+      formatOption () {
+        this.option = {
+          title: {
+            text: 'Graph 简单示例'
+          },
+          tooltip: {},
+          animationDurationUpdate: 1500,
+          animationEasingUpdate: 'quinticInOut',
+          series : [
+            {
+              type: 'graph',
+              layout: 'none',
+              symbolSize: 50,
+              roam: 'scale',
+              label: {
+                normal: {
+                  show: true
+                }
+              },
+              edgeSymbol: ['circle', 'arrow'],
+              edgeSymbolSize: [4, 10],
+              edgeLabel: {
+                normal: {
+                  textStyle: {
+                    fontSize: 20
+                  }
+                }
+              },
+              nodes: this.graph.nodes,
+              links: this.graph.links,
+              lineStyle: {
+                normal: {
+                  opacity: 0.9,
+                  width: 2,
+                  curveness: 0
+                }
+              }
+            }
+          ]
+        };
+        this.myChart.hideLoading();
+        this.myChart.setOption(this.option, true);
+      },
+      initDrag () {
+        this.option.graphic = echarts.util.map(this.graph.nodes, (item, dataIndex) => {
+          return {
+            type: 'circle',
+            position: this.myChart.convertToPixel({seriesIndex: 0}, [item.x, item.y]),
+            shape: {
+              r: 10
+            },
+            // invisible: true,
+            style: {
+              fill: 'blue'
+            },
+            draggable: true,
+            ondrag: echarts.util.curry(this.onPointDragging, dataIndex),
+            z: 100
+          }
+        });
+        this.myChart.setOption(this.option);
+        window.addEventListener('resize', this.updatePosition);
+      },
+      updatePosition () {
+        this.myChart.setOption({
+          graphic: echarts.util.map(this.graph.nodes, (item, dataIndex) => {
+            return {
+              position: this.myChart.convertToPixel({seriesIndex: 0}, [item.x, item.y])
+            };
+          })
+        });
+      },
+      onPointDragging (dataIndex, event) {
+        let pos = this.myChart.convertFromPixel({seriesIndex: 0}, [event.offsetX, event.offsetY]) // 将graphic的像素坐标转化为坐标系坐标
+        this.graph.nodes[dataIndex].x = pos[0] // 将新的坐标系坐标赋值给node
+        this.graph.nodes[dataIndex].y = pos[1]
+        this.myChart.setOption({
+          series: {
+            nodes: this.graph.nodes
+          }
+        });
+        this.updatePosition()
+      }
+    }
+  }
+</script>
+<style scoped>
+  #charts {
+    flex: 1;
+    border: 1px solid;
+    box-shadow: 0 0 20px rgba(18, 208, 253, .5);
+  }
+</style>
